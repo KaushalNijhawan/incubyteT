7afc3474d24c8aa078e5ee4114da4715ad21e2b6
kkaushalnijhawan <kaushal.nijhawan@gromo.in>
2024-06-09 18:46:35 +0530

    handeling large input values

--- src/App.java
+++ src/App.java
@@ -1,14 +1,14 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.math.BigInteger;
 public class App {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the numbers below you want to add !");
         String numbers  = br.readLine();
         try {
-            System.out.println(add("//;\n1;2,-1,2,-2"));    
+            System.out.println(add("//;\n1;2,-1,2"));    
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -26,29 +26,28 @@ public class App {
         if (numbers.contains(";")) {
             numbers = numbers.replaceAll(";", ",");
         }        
-        int addVal = 0 ;
         numbers = numbers.replaceAll("\n", ",");
         String[] str = numbers.trim().split(",");
 
         // check for delimetres
         
         StringBuilder negativeNumberString = new StringBuilder("");
-
+        BigInteger finValue = new BigInteger("0");
         for (String val : str) {
             if (val.equals("")) {
-                addVal += 0;
+                finValue.add(new BigInteger("0"));
             }else{
                 int valCurrent = Integer.parseInt(val);
                 if (valCurrent < 0 ) {
                     negativeNumberString.append(valCurrent);
                     negativeNumberString.append(",");
                 }
-                addVal += valCurrent;
+                finValue.add(new BigInteger(val));
             }
         }
         if (negativeNumberString.length() > 0 ) {
             throw new NumberFormatException("negative numbers not allowed " + negativeNumberString.toString().substring(0, negativeNumberString.length()-1));
         }
-        return addVal;
+        return finValue.intValue();
     }
 }
--- src/AppTest.java
+++ src/AppTest.java
@@ -1,5 +1,3 @@
-import static org.junit.Assert.assertThrows;
-
 import org.junit.*;
 
 public class AppTest{
